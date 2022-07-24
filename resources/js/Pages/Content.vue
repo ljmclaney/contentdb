761664113280a3e8c072585f8bb2022a50e388af
Liam Mclaney <ljmclaney@gmail.com>
2022-07-24 20:06:29 +0100

    Initial commit

---
+++ resources/js/Pages/Content.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="max-w-3xl mx-auto px-4 py-[50px]">
+
+        <div class="bg-white rounded shadow border border-gray-200">
+            <div class="flex items-center border-b border-gray-200 p-5">
+                <h3 class="font-bold">Header title</h3>
+            </div>
+            <div class="p-5">
+                <div class="flex mb-5 space-x-2" v-if="editor">
+                    <span class="rbg-white  elative z-0 inline-flex shadow-sm rounded border border-gray-300">
+                    <button class="inline-flex items-center px-2.5 py-1.5 border-r border-gray-300 text-xs font-medium text-gray-700 hover:bg-gray-50" :class="{ 'is-active': editor.isActive('bold') }" @click="editor.chain().focus().toggleBold().run()">
+                        Bold
+                    </button>
+                    <button class="inline-flex items-center px-2.5 py-1.5 text-xs font-medium text-gray-700 hover:bg-gray-50" :class="{ 'is-active': editor.isActive('italic') }" @click="editor.chain().focus().toggleItalic().run()">
+                        Italic
+                    </button>
+                    </span>
+                </div>
+                <editor-content :editor="editor" class="focus:ring-0" />
+            </div>
+            <div class="flex items-center border-t border-gray-200 p-5">
+                <p class="text-sm text-gray-500">Useful help text</p>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+    components: {
+        EditorContent,
+    },
+
+    data() {
+        return {
+            editor: null,
+        }
+    },
+
+    mounted() {
+        this.editor = new Editor({
+            content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+            extensions: [
+                StarterKit.configure({
+                    heading: {
+                        levels: [1, 2],
+                    }
+                })
+            ],
+        })
+    },
+
+    beforeUnmount() {
+        this.editor.destroy()
+    },
+}
+</script>
---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import {homedir} from 'os'
+import {resolve} from 'path'
+import fs from 'fs';
+
+let host = 'contentdb.test'
+
+export default defineConfig({
+    server: detectServerConfig(host),
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
