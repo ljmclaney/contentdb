4924a4d89395d96acd442fc28e8a8670030bf296
Liam Mclaney <ljmclaney@gmail.com>
2022-07-31 15:06:57 +0100

    Update migrations

--- database/migrations/2022_07_25_124444_create_accounts_table.php
+++ database/migrations/2022_07_25_124444_create_accounts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->string('stripe_id')->nullable()->index();
+            $table->string('pm_type')->nullable();
+            $table->string('pm_last_four', 4)->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
             $table->timestamps();
         });
     }
