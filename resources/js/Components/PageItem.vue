7b56fca0ca447d86c8c79bc9dcac209a504433c5
Liam Mclaney <ljmclaney@gmail.com>
2022-08-05 15:40:53 +0100

    Update PageItem.vue

--- resources/js/Components/PageItem.vue
+++ resources/js/Components/PageItem.vue
@@ -2,9 +2,15 @@
     <li>
         <div class="flex items-center justify-between">
             <div class="whitespace-nowrap py-4 pl-4 pr-3 font-medium sm:pl-6 md:pl-0 w-3/4 truncate">
-                <Link :href="route('viewPage', [project.id, node.id])" class="text-indigo-600 hover:text-indigo-900 truncate">
+                <Link :href="route('viewPage', [project.id, node.id])" class="text-indigo-600 hover:text-indigo-900 truncate inline-flex items-center">
 
-                    <span v-if="node.depth" v-for="n in node.depth">-</span>
+                    <span v-if="node.depth" class="flex items-center mr-2">
+                        <span v-for="n in node.depth">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
+                            </svg>
+                        </span>
+                    </span>
 
                     {{ node.name }}
                 </Link>
