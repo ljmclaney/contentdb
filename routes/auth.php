0c805076024de80f4b8de2a1251a0060a8043a59
Liam Mclaney <ljmclaney@gmail.com>
2022-08-10 19:57:56 +0100

    Add google login

--- routes/auth.php
+++ routes/auth.php
@@ -19,9 +19,6 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::get('login-new', [AuthenticatedSessionController::class, 'new'])
-        ->name('login-new');
-
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
