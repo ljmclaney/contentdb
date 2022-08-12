86999e0c24f55f66afea351cc3e2b7e4985ad8af
Liam Mclaney <ljmclaney@gmail.com>
2022-08-12 21:15:36 +0100

    Add migrations

---
+++ database/migrations/2022_08_12_194535_create_reminders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('client_id');
+            $table->date('reminder_date');
+            $table->longText('reminder_message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reminders');
+    }
+};
